6069ce0dd9f5d75bd04dbf45a254a133e0bf49c7
Chris Lam <chris@chrislam.nl>
2022-03-08 10:55:14 +0100

    use mean, fit overwrites

--- notebooks/LinearRegression.py
+++ notebooks/LinearRegression.py
@@ -11,15 +11,14 @@ class LinearRegression:
   def fit(self, X, y, N=1000):
     if len(X) != len(y):
       raise Exception("Inputs X and y not the same length.")
-    if self.weights is None:
-      self.weights = np.random.rand(X.shape[1])
-      self.bias = random.random()
+    self.weights = np.random.rand(X.shape[1])
+    self.bias = random.random()
     for _ in (t := trange(N)):
       p = self.predict(X)
       loss = np.mean((p - y) ** 2)
-      t.set_description(f"Loss: {loss}, Progress:")
-      self.weights -= self.lr * np.sum((self.weights * (2 * (p - y)))) / len(X)
-      self.bias -= self.lr * np.sum(2 * self.bias) / len(X)
+      t.set_description(f"Loss: {loss}, Progress")
+      self.weights -= self.lr * np.mean((self.weights * (2 * (p - y))))
+      self.bias -= self.lr * np.mean(2 * self.bias)
 
   def predict(self, X):
     return self.weights * X + self.bias
@@ -30,7 +29,7 @@ def format_input(a):
 if __name__ == "__main__":
   X_train = [1, 2, 3, 5, 6, 7, 8]
   y_train = [10, 20, 30, 50, 60, 70, 80]
-  X_test = [1, 8, 11]
+  X_test = [1, 8, 15, 120]
 
   X_train = format_input(X_train)
   y_train = format_input(y_train)
@@ -38,6 +37,5 @@ if __name__ == "__main__":
 
   model = LinearRegression(lr=0.01)
   model.fit(X_train, y_train)
-  p = model.predict(X_test)
-  print(p)
-
+  print(model.predict(X_test))
+  
